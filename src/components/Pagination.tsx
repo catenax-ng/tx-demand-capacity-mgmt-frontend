66cb0e410f36f1b6164ef455c49f3476c157090b
Pedro Bailão <p.bailao@campus.fct.unl.pt>
2023-07-06 13:07:56 +0100

    Update Pagination.tsx

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,23 +1,5 @@
-/********************************************************************************
- * Copyright (c) 2021,2023 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import Pagination from 'react-bootstrap/Pagination';
 
 interface PaginationProps {
   pages: number;
@@ -26,7 +8,12 @@ interface PaginationProps {
   demands: any[]; // Update with the correct type for demands
 }
 
-const Pagination: React.FC<PaginationProps> = ({ pages, setCurrentPage, currentDemands, demands }) => {
+const CustomPagination: React.FC<PaginationProps> = ({
+  pages,
+  setCurrentPage,
+  currentDemands,
+  demands,
+}) => {
   const numOfPages: number[] = [];
 
   for (let i = 1; i <= pages; i++) {
@@ -39,28 +26,37 @@ const Pagination: React.FC<PaginationProps> = ({ pages, setCurrentPage, currentD
     setCurrentPage(currentButton);
   }, [currentButton, setCurrentPage]);
 
+  const handlePrevious = () => {
+    setCurrentButton((prev) => (prev === 1 ? prev : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentButton((next) => (next === numOfPages.length ? next : next + 1));
+  };
+
   return (
     <div className="clearfix">
       <div className="hint-text">
         Showing <b>{currentDemands.length}</b> out of <b>{demands.length}</b> entries
       </div>
-      <ul className="pagination">
-        <li className={`${currentButton === 1 ? 'page-item disabled' : 'page-item'}`}>
-          <a href="#!" onClick={() => setCurrentButton((prev) => (prev === 1 ? prev : prev - 1))}>Previous</a>
-        </li>
-        {numOfPages.map((page, index) => {
-          return (
-            <li key={index} className={`${currentButton === page ? 'page-item active' : 'page-item'}`}>
-              <a href="#!" className="page-link" onClick={() => setCurrentButton(page)}>{page}</a>
-            </li>
-          )
-        })}
-        <li className={`${currentButton === numOfPages.length ? 'page-item disabled' : 'page-item'}`}>
-          <a href="#!" onClick={() => setCurrentButton((next) => (next === numOfPages.length ? next : next + 1))}>Next</a>
-        </li>
-      </ul>
+      <Pagination>
+        <Pagination.Prev disabled={currentButton === 1} onClick={handlePrevious} />
+        {numOfPages.map((page, index) => (
+          <Pagination.Item
+            key={index}
+            active={currentButton === page}
+            onClick={() => setCurrentButton(page)}
+          >
+            {page}
+          </Pagination.Item>
+        ))}
+        <Pagination.Next
+          disabled={currentButton === numOfPages.length}
+          onClick={handleNext}
+        />
+      </Pagination>
     </div>
   );
 };
 
-export default Pagination;
+export default CustomPagination;
